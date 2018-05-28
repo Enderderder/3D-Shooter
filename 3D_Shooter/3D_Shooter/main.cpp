5354c6adf2c282e60741314669d7a9365d1d805e
Enderderder <richardwulansari@gmail.com>
2018-05-28 15:23:57 +1200

    Hello

--- 3D_Shooter/3D_Shooter/main.cpp
+++ 3D_Shooter/3D_Shooter/main.cpp
@@ -22,6 +22,8 @@ void ResizeWindow(int _width, int _height);
 
 int main(int argc, char **argv)
 {
+	// Hello there
+
 	// Create the window
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA | GL_MULTISAMPLE);
