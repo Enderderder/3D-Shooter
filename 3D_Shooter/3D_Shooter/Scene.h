7c6cb93a3ee94e57d13322bf6c77cdd5ad215052
JacobD <jacob.dewse@gmail.com>
2018-05-30 22:56:45 +1200

    -Added Camera Class
    -added camera class
    -edited scene.cpp to have dynamic shader use

--- 3D_Shooter/3D_Shooter/Scene.h
+++ 3D_Shooter/3D_Shooter/Scene.h
@@ -1,6 +1,14 @@
 #ifndef _SCENE_H
 #define _SCENE_H
 
+
+
+// Local Include
+
+// Global Include
+#include "TextLabel.h"
+#include "Camera.h"
+
 // Forward Declaration
 enum ESCENES; 
 
@@ -14,6 +22,26 @@ public:
 	void InitialiseScene();
 	void RenderScene();
 	void UpdateScene();
+
+private:
+	Camera m_Camera;
+	GLuint vbo;
+	GLuint vao;
+	GLuint ebo;
+
+	////////////////USE FOR CAMERA CLASS THEN PASSED INTO SHADERS///////////////////////////////////
+
+	glm::mat4 m_TranslationMatrix = glm::mat4();
+	glm::mat4 m_IdentityMatrix = glm::mat4();	//Defualt Matrix
+	glm::mat4 m_RotationMatrix = glm::mat4();
+	glm::mat4 m_ScaleMatrix = glm::mat4();
+	glm::mat4 m_ModelMatrix = glm::mat4();
+	glm::mat4 m_MVPMatrix = glm::mat4();
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	const char* VertexShader;
+	const char* FragmentShader;
 };
 
 #endif // !_SCENE_H
