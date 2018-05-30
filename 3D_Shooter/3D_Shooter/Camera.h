c5ec0b7f36ce4ec251c3f9fc36d9e462454001c8
Enderderder <richardwulansari@gmail.com>
2018-05-31 01:19:02 +1200

    ~ Copyed Camera.h from Jacob's branch

--- 3D_Shooter/3D_Shooter/Camera.h
+++ 3D_Shooter/3D_Shooter/Camera.h
@@ -1,10 +1,40 @@
-
-
-
-class CCamera
+#pragma once
+#include "Utility.h"
+class Camera
 {
 public:
-	CCamera();
-	~CCamera();
-};
+	Camera();
+	~Camera();
 
+
+	Camera(glm::vec3 _Positition, glm::vec3 _Facing, glm::vec3 _Normal);
+	void Update();
+
+	glm::mat4 GetViewMatrix();
+	void SetViewMatrix();
+	void SetViewMatrix(glm::vec3 _Position, glm::vec3 _Facing, glm::vec3 _Normal);
+
+	glm::mat4 GetProjectionMatrix();
+	void SetProjectionMatrix();
+
+	glm::vec3 GetCameraPosition();
+	void SetCameraPosition(glm::vec3 _Position);
+
+	glm::vec3 GetCameraFacing();
+	void SetCameraFacing(glm::vec3 _Facing);
+
+	glm::vec3 GetCameraNormal();
+	void SetCameraNormal(glm::vec3 _Normal);
+
+
+
+
+private:
+	glm::vec3 m_CameraPosition = glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 m_CameraFacing = glm::vec3(0.0f, 0.0f, -1.0f);
+	glm::vec3 m_CameraNormal = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::mat4 m_ViewMatrix;
+	glm::mat4 m_ProjectionMatrix;
+
+
+};
